f0ee2dacd565d9198d32b8c1b9cde3c49eee1c3d
Juan Sanchez <juan.sanchez@qrvey.com>
2021-05-02 14:43:14 -0500

    add toasts messages

--- app/src/main/java/com/example/pinterest_final/MainActivity.kt
+++ app/src/main/java/com/example/pinterest_final/MainActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
@@ -24,7 +26,21 @@ class MainActivity : AppCompatActivity() {
         btnLogin.setOnClickListener {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
+
         }
 
+        val btnFacebook: Button = findViewById(R.id.btn_facebook)
+        val btnGoogle: Button = findViewById(R.id.btn_google)
+        val btnApple: Button = findViewById(R.id.btn_apple)
+
+        btnFacebook.setOnClickListener(this::handler)
+        btnGoogle.setOnClickListener(this::handler)
+        btnApple.setOnClickListener(this::handler)
+    }
+
+    fun handler(view: View?) : Unit {
+        val btn = view as Button
+        val toast = Toast.makeText(applicationContext, getString(R.string.lbl_you_selected) + ": " + btn.text, Toast.LENGTH_SHORT)
+        toast.show()
     }
 }

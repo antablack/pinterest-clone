a34d77d5bc783cdfe90ee8068833e109d48623b0
Juan Sanchez <juan.sanchez@qrvey.com>
2021-05-02 14:56:27 -0500

    add toasts messages to images views

--- app/src/main/java/com/example/pinterest_final/DashboardActivity.kt
+++ app/src/main/java/com/example/pinterest_final/DashboardActivity.kt
@@ -2,7 +2,11 @@ package com.example.pinterest_final
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.makeramen.roundedimageview.RoundedTransformationBuilder
 
@@ -23,5 +27,36 @@ class DashboardActivity : AppCompatActivity() {
             .load(R.drawable.bedroom)
             .transform(transformation)
             .into(imageBedroom);*/
+        findViewById<ImageView>(R.id.image_bedroom).setOnClickListener(this::handler)
+        findViewById<ImageView>(R.id.image_livingroom).setOnClickListener(this::handler)
+        findViewById<ImageView>(R.id.image_bathroom).setOnClickListener(this::handler)
+        findViewById<ImageView>(R.id.image_outdoor).setOnClickListener(this::handler)
+        findViewById<ImageView>(R.id.image_homeoffice).setOnClickListener(this::handler)
+        findViewById<ImageView>(R.id.image_kitchen).setOnClickListener(this::handler)
+    }
+
+    fun handler(view: View?) : Unit {
+        lateinit var toast: Toast;
+        when (view?.id) {
+            R.id.image_bedroom -> {
+                toast = Toast.makeText(applicationContext, getString(R.string.lbl_you_selected) + ": " + findViewById<TextView>(R.id.textView_bedroom).text , Toast.LENGTH_SHORT)
+            }
+            R.id.image_livingroom -> {
+                toast = Toast.makeText(applicationContext, getString(R.string.lbl_you_selected) + ": " + findViewById<TextView>(R.id.textView_livingroom).text , Toast.LENGTH_SHORT)
+            }
+            R.id.image_bathroom -> {
+                toast = Toast.makeText(applicationContext, getString(R.string.lbl_you_selected) + ": " + findViewById<TextView>(R.id.textView_bathroom).text , Toast.LENGTH_SHORT)
+            }
+            R.id.image_outdoor -> {
+                toast = Toast.makeText(applicationContext, getString(R.string.lbl_you_selected) + ": " + findViewById<TextView>(R.id.textView_outdoor).text , Toast.LENGTH_SHORT)
+            }
+            R.id.image_homeoffice -> {
+                toast = Toast.makeText(applicationContext, getString(R.string.lbl_you_selected) + ": " + findViewById<TextView>(R.id.textView_homeoffice).text , Toast.LENGTH_SHORT)
+            }
+            R.id.image_kitchen -> {
+                toast = Toast.makeText(applicationContext, getString(R.string.lbl_you_selected) + ": " + findViewById<TextView>(R.id.textView_kitchen).text , Toast.LENGTH_SHORT)
+            }
+        }
+        toast.show()
     }
 }

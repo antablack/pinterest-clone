d807b8c240eef1e0a168724bebd642c791ed9c21
Juan Sanchez <juan.sanchez@qrvey.com>
2021-05-01 20:53:15 -0500

    refactor design

--- app/src/main/java/com/example/pinterest_final/DashboardActivity.kt
+++ app/src/main/java/com/example/pinterest_final/DashboardActivity.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.makeramen.roundedimageview.RoundedTransformationBuilder
-import com.squareup.picasso.Picasso
-import com.squareup.picasso.Transformation
 
 
 class DashboardActivity : AppCompatActivity() {
--- app/src/main/java/com/example/pinterest_final/SignUp.kt
+++ app/src/main/java/com/example/pinterest_final/SignUp.kt
@@ -1,11 +1,24 @@
 package com.example.pinterest_final
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        val edittextEmail = findViewById<EditText>(R.id.edittext_email)
+
+        edittextEmail.requestFocus()
+
+        val btnNext = findViewById<Button>(R.id.btn_next)
+
+        btnNext.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
